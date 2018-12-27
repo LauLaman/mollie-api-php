a887331d68c1dcdaab6e50070debd528f806c752
Laurens Laman <laurens.laman@llaman.nl>
2018-12-28 00:01:14 +0100

    fix construction of empty ChargebackCollection

--- src/Resources/Payment.php
+++ src/Resources/Payment.php
@@ -496,7 +496,7 @@ class Payment extends BaseResource
     public function chargebacks()
     {
         if (!isset($this->_links->chargebacks->href)) {
-            return new ChargebackCollection(0, null);
+            return new ChargebackCollection($this->client, null, null);
         }
 
         $result = $this->client->performHttpCallToFullUrl(
